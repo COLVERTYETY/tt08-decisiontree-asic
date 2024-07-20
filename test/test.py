2947d40da077fb268b3ae5bf5c94d26880d68045
nicolas <nicolas.jan.stas@gmail.com>
2024-07-20 14:28:59 -0700

    added test functions

--- test/test.py
+++ test/test.py
@@ -1,10 +1,46 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
-
+import os 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+
+def get_dataset():
+    abs_path = os.path.abspath(__file__)
+    rel_path = "../src/amaranth/data.txt"
+    path = os.path.join(os.path.dirname(abs_path), rel_path)
+    with open(path, "r") as file:
+        data = file.readlines()
+
+    samples = []
+    for i in range(0, len(data), 7):
+        label = data[i].strip()
+        values = data[i+1:i+6]
+        samples.append((label, values))
+
+    dataset = []
+    for label, values in samples:
+        if len(label)!=1 or len(values)<5:
+            continue
+        l = int(label)
+        values = [[1 if c == 'x' else 0 for c in row.strip()] for row in values]
+        dataset.append((l, values))
+    return dataset
+
+def boolarray_to_int(x):
+    x_ = 0
+    for i, bit in enumerate(x):
+        x_ += bit*2**i
+    return x_
+
+def shape_input(x):
+    shape_data = x
+    data =[]
+    for i,d in enumerate(shape_data):
+        x_ = boolarray_to_int(d)
+        data.append((i,x_))
+    return data
 
 @cocotb.test()
 async def test_project(dut):
